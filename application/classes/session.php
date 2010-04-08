fe9336080707a8e97eaf81ec1e3f2db5de2a9a50
Geert De Deckere <geert@idoe.be>
2010-04-08 19:37:13 +0200

    Updated submodules and default database sessions

--- application/classes/session.php
+++ application/classes/session.php
@@ -2,10 +2,7 @@
 
 abstract class Session extends Kohana_Session {
 
-	public static function instance($type = NULL, $id = NULL)
-	{
-		// Force database sessions by default
-		return parent::instance('database', $id);
-	}
+	// Use database sessions by default
+	protected static $type = 'database';
 
 }
