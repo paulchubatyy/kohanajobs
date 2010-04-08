0ec841d80a05b9368c3603657b29a97064be2e2b
Geert De Deckere <geert@idoe.be>
2010-04-08 13:55:41 +0200

    Force database sessions

---
+++ application/classes/session.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Session extends Kohana_Session {
+
+	public static function instance($type = NULL, $id = NULL)
+	{
+		// Force database sessions by default
+		return parent::instance('database', $id);
+	}
+
+} 
