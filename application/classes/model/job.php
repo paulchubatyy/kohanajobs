589b720f2a72458d823fc7665b4cd64fd87f8765
Geert De Deckere <geert@idoe.be>
2010-04-08 11:36:42 +0200

    A few minor cleanups

--- application/classes/model/job.php
+++ application/classes/model/job.php
@@ -5,15 +5,15 @@ class Model_Job extends ORM {
 	// Auto-update column for creation
 	protected $_created_column = array(
 		'column' => 'created',
-		'format' => TRUE
+		'format' => TRUE,
 	);
 
 	// Validation rules
 	protected $_rules = array(
-		'company'  => array('not_empty' => NULL),
-		'location' => array('not_empty' => NULL),
-		'website'  => array('url' => NULL),
-		'email'    => array('not_empty' => NULL, 'email' => NULL),
+		'company'     => array('not_empty' => NULL),
+		'location'    => array('not_empty' => NULL),
+		'website'     => array('url' => NULL),
+		'email'       => array('not_empty' => NULL, 'email' => NULL),
 		'title'       => array('not_empty' => NULL),
 		'description' => array('not_empty' => NULL),
 		'apply'       => array('not_empty' => NULL),
@@ -21,7 +21,7 @@ class Model_Job extends ORM {
 
 	// Input filter
 	protected $_filters = array(
-		TRUE => array('trim' => NULL)
+		TRUE => array('trim' => NULL),
 	);
 
 }
