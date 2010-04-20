380cf99a41881b332aace158301afed2559943d9
Geert De Deckere <geert@idoe.be>
2010-04-20 09:38:15 +0200

    Session::$type is public now

--- application/classes/session.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-abstract class Session extends Kohana_Session {
-
-	// Use database sessions by default
-	protected static $type = 'database';
-
-}
