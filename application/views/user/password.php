81b41cc3d45854bf73589bbbc5e7cdae2485ed29
Geert De Deckere <geert@idoe.be>
2010-04-20 09:35:10 +0200

    Password form view

---
+++ application/views/user/password.php
@@ -0,0 +1,26 @@
+<h1>Change Password</h1>
+
+<?php echo Form::open() ?>
+
+	<?php echo Kohana::debug($errors) ?>
+
+	<p>
+		<?php echo Form::label('old_password', 'Current password:') ?>
+		<?php echo Form::password('old_password', NULL, array('id' => 'old_password')) ?>
+	</p>
+
+	<p>
+		<?php echo Form::label('password', 'New password:') ?>
+		<?php echo Form::password('password', NULL, array('id' => 'password')) ?>
+	</p>
+
+	<p>
+		<?php echo Form::label('password_confirm', 'New password again:') ?>
+		<?php echo Form::password('password_confirm', NULL, array('id' => 'password_confirm')) ?>
+	</p>
+
+	<p>
+		<?php echo Form::submit(NULL, 'Change password') ?>
+	</p>
+
+<?php echo Form::close() ?>
